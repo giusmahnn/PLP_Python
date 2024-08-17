af29c749fd04895fec1120ad60eb422b1399ab41
remigiusM <garethremigius@gmail.com>
2024-08-17 19:13:59 +0100

    Done

---
+++ Week_Two/list.py
@@ -0,0 +1,23 @@
+mylist = []
+
+# To append an item to the list
+mylist.append(10)
+mylist.append(20)
+mylist.append(30)
+mylist.append(40)
+
+# To insert an item at a specific index
+mylist.insert(1, 15)
+
+# To extend the list
+mylist.extend([50, 60, 70])
+
+# To remove an item
+mylist.pop()
+
+# To sort a list
+mylist.sort()
+
+# You can uncomment the following
+print(mylist)
+# print(mylist.index(30))
